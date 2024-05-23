7e74086031214a125811f3141f759792a19f3a87
matthew aboyade <aboyadematthew@gmail.com>
2024-05-22 18:46:03 -0700

    add functions to form and story  of letter one

--- js/script.js
+++ js/script.js
@@ -1,1 +1,51 @@
-$(document)
+$(document).ready(function () {
+    $("#form").submit(function (event) {
+        const name1Taken = $("#name1").val();
+        const name2Taken = $("#name2").val();
+        const schNameTaken = $("#sch-name").val();
+        const intrestTaken = $("#intrest").val();
+        const emailTakent = $("#email").val();
+        const numberTaken = $("#number").val();
+
+        $(".name1").html(name1Taken);
+        $(".name2").html(name2Taken);
+        $(".sch-name").html(schNameTaken);
+        $(".intrest").html(intrestTaken);
+        $(".email").html(emailTakent);
+        $(".number").html(numberTaken);
+
+
+
+        $("#mail").show();
+        $("#form").hide();
+        $("#main").show();
+        $(".gen").show();
+
+        event.preventDefault();
+    });
+    $("#sch").click(function () {
+        $(".main-container").toggle();
+    });
+
+    $("#edit").click(function () {
+        $("#main").hide();
+        $("#form").show();
+    });
+
+    $("#new").click(function () {
+        $("#name1").val('');
+        $("#name2").val('');
+        $("#sch-name").val('');
+        $("#intrest").val('');
+        $("#email").val('');
+        $("#number").val('');
+
+        $(".gen").hide();
+        $("#main").hide();
+        $("#mail").hide();
+        $("#form").show();
+    });
+
+
+});
+
