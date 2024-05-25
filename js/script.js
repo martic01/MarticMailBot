254c9c000b5c202adc3add57801bf102c555b9a5
matthew aboyade <aboyadematthew@gmail.com>
2024-05-25 08:41:34 -0700

    add copy button function in js

--- js/script.js
+++ js/script.js
@@ -22,7 +22,9 @@ $(document).ready(function () {
         $("#form").hide();
         $("#main").show();
         $(".count").show();
-        $("#edit").show();
+        $("#cop1").show();
+        $("#cop2").hide();
+        $("#copying1").show();
         $("#h12").hide();
         $("#but").show();
         $("#fileinspan").toggle();
@@ -39,6 +41,8 @@ $(document).ready(function () {
         $("#form2").hide();
         $("#form3").hide();
         $("#mail").hide();
+        $("#main").hide();
+        $("#copying1").hide();
         $(".count").hide();
         $("#h12").hide();
         $("#but").show();
@@ -62,7 +66,9 @@ $(document).ready(function () {
         $("#fileinspan").toggle();
         $("#file-span").toggle();
         $(".count").toggle();
-        $("#edit").show();
+        $("#copying1").toggle();
+        $("#cop1").show();
+        $("#cop2").hide();
         $("#main").hide();
 
 
@@ -77,6 +83,21 @@ $(document).ready(function () {
 
     });
 
+    $("#copy1").click(function () {
+        let textToCopy = $("#mail").text();
+        let tempTextarea = $("<textarea>");
+        tempTextarea.val(textToCopy);
+        $("body").append(tempTextarea);
+        tempTextarea.select();
+        document.execCommand("copy");
+        $(tempTextarea).remove();
+
+
+        $("#cop1").hide();
+        $("#cop2").show();
+        // $("#mail2").addClass(".copying");
+
+    });
 
 
     $("#edit").click(function () {
@@ -84,18 +105,23 @@ $(document).ready(function () {
         $("#but").hide();
         $("#main").hide();
         $("#form").show();
-        $(".count").toggle();
+        $(".count").show();
         $("#fileinspan").toggle();
         $("#file-span").toggle()
+        $("#cop1").show();
+        $("#cop2").hide();
     });
 
     $("#new").click(function () {
         $(".count").hide();
-        $("#main").show();
+        $("#main").hide();
         $("#mail").hide();
         $("#form").show();
         $("#fileinspan").toggle();
         $("#file-span").toggle()
+        $("#copying1").hide();
+        $("#cop1").show();
+        $("#cop2").hide();
 
 
         $("#name1").val('');
@@ -107,6 +133,7 @@ $(document).ready(function () {
     });
 
 
+    // SECOUND FORM
 
     $("#form2").submit(function (event) {
         const value1Taken = $("#manager-nm").val();
@@ -128,7 +155,9 @@ $(document).ready(function () {
         $("#form2").hide();
         $("#main2").show();
         $(".count").show();
-        $("#edit2").show();
+        $("#cop4").show();
+        $("#cop5").hide();
+        $("#copying2").show();
         $("#h13").hide();
         $("#but2").show();
         $("#fileinspan2").toggle();
@@ -151,6 +180,8 @@ $(document).ready(function () {
         $("#but2").show();
         $("#fileinspan2").show();
         $("#file-span2").hide();
+        $("#copying2").hide();
+
 
 
         $("#manager-nm").val("");
@@ -168,8 +199,10 @@ $(document).ready(function () {
         $("#fileinspan2").toggle();
         $("#file-span2").toggle();
         $(".count").toggle();
-        $("#edit2").show();
         $("#main2").hide();
+        $("#copying2").toggle();
+        $("#cop4").show();
+        $("#cop5").hide();
 
         $(".manager-nm").html("[Manager's Name]");
         $(".yrname").html("[Your Name]");
@@ -180,22 +213,43 @@ $(document).ready(function () {
 
     });
 
+    $("#copy2").click(function () {
+        let textToCopy = $("#mail2").text();
+        let tempTextarea = $("<textarea>");
+        tempTextarea.val(textToCopy);
+        $("body").append(tempTextarea);
+        tempTextarea.select();
+        document.execCommand("copy");
+        $(tempTextarea).remove();
+
+
+        $("#cop4").hide();
+        $("#cop5").show();
+        // $("#mail2").addClass(".copying");
+
+    });
+
 
     $("#edit2").click(function () {
         $("#h13").show();
         $("#but2").hide();
         $("#main2").hide();
         $("#form2").show();
-        $(".count").toggle();
+        $(".count").show();
+        $("#cop4").show();
+        $("#cop5").hide();
         $("#fileinspan2").toggle();
         $("#file-span2").toggle()
     });
 
     $("#new2").click(function () {
         $(".count").hide();
-        $("#main2").show();
+        $("#main2").hide();
         $("#mail2").hide();
         $("#form2").show();
+        $("#copying2").hide();
+        $("#cop4").show();
+        $("#cop5").hide();
         $("#fileinspan2").toggle();
         $("#file-span2").toggle()
 
@@ -203,9 +257,10 @@ $(document).ready(function () {
         $("#yrname").val("");
         $("#company").val("");
         $("#yrposition").val("");
-        $("#nw-date").val("");
     });
 
+
+    // THEIRD FORM
 
     $("#form3").submit(function (event) {
         const value1Taken = $("#employ-name").val();
@@ -234,7 +289,9 @@ $(document).ready(function () {
         $("#form3").hide();
         $("#main3").show();
         $(".count").show();
-        $("#edit3").show();
+        $("#cop6").show();
+        $("#cop7").hide();
+        $("#copying3").show();
         $("#h14").hide();
         $("#but3").show();
         $("#fileinspan3").toggle();
@@ -256,6 +313,7 @@ $(document).ready(function () {
         $("#but3").show();
         $("#fileinspan3").show();
         $("#file-span3").hide();
+        $("#copying3").hide();
 
 
 
@@ -279,6 +337,9 @@ $(document).ready(function () {
         $(".count").toggle();
         $("#edit3").show();
         $("#main3").hide();
+        $("#copying3").toggle();
+        $("#cop6").show();
+        $("#cop7").hide();
 
 
 
@@ -294,6 +355,22 @@ $(document).ready(function () {
 
     });
 
+    $("#copy3").click(function () {
+        let textToCopy = $("#mail3").text();
+        let tempTextarea = $("<textarea>");
+        tempTextarea.val(textToCopy);
+        $("body").append(tempTextarea);
+        tempTextarea.select();
+        document.execCommand("copy");
+        $(tempTextarea).remove();
+
+
+        $("#cop6").hide();
+        $("#cop7").show();
+        // $("#mail2").addClass(".copying");
+
+    });
+
 
 
     $("#edit3").click(function () {
@@ -301,18 +378,23 @@ $(document).ready(function () {
         $("#but3").hide();
         $("#main3").hide();
         $("#form3").show();
-        $(".count").toggle();
+        $(".count").show();
+        $("#cop6").show();
+        $("#cop7").hide();
         $("#fileinspan3").toggle();
         $("#file-span3").toggle()
     });
 
     $("#new3").click(function () {
         $(".count").hide();
-        $("#main3").show();
+        $("#main3").hide();
         $("#mail3").hide();
         $("#form3").show();
         $("#fileinspan3").toggle();
-        $("#file-span3").toggle()
+        $("#file-span3").toggle();
+        $("#copying3").hide();
+        $("#cop6").show();
+        $("#cop7").hide();
 
 
         $("#employ-name").val("");
@@ -323,7 +405,6 @@ $(document).ready(function () {
         $("#name-company").val("");
         $("#yr-position").val("");
         $("#reason").val("");
-        $("#day").val("");
     });
 
 });
